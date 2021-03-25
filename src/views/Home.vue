01b7bb53626a033cebff6db4bf9a671f633b3137
romanbatavi <romanbatavi98@gmail.com>
2021-03-25 16:13:24 +0700

    first ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <section class="vote">
+      <h3 class="give-ur-vote">
+        Silahkan Berikan Penilaian
+      </h3>
+      <div class="emoticons">
+        //Akan Menjadi Emoticon
+      </div>
+    </section>
+    <section class="finish">
+      <h4>Terimakasih Atas Penilaian Anda
+      </h4>
+    </section>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+
+<style type="text/css">
+  .container{
+    display: flex;
+    height: 100hv;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
+  section{
+    width: 100%;
+  }
+</style>
