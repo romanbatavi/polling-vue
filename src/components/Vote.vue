21c33b20c8d795e5a28bace129add70d38153342
romanbatavi <romanbatavi98@gmail.com>
2021-03-29 12:38:48 +0700

    fix css/style emoticons

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -32,5 +32,64 @@ export default {
     background: url('~@/assets/emoticon.png');
     width: 101px;
     height: 100px;
+    border: none;
+    margin: 0px 10px;
+    outline: none;
+    cursor: pointer;
+  }
+
+  .emoticons{
+    display: flex;
+    justify-content: center;
+  }
+  /* very-bad */
+  #very-bad{
+    background-position: 0px 0px;
+  }
+  #very-bad:hover{
+    background-position: 0px -100px;
+  }
+  #very-bad:active{
+    background-position: 0px -200px;
+  }
+  /* bad */
+  #bad{
+    background-position: -101px 0px;
+  }
+  #bad:hover{
+    background-position: -101px -100px;
+  }
+  #bad:active{
+    background-position: -101px -200px;
+  }
+  /* ok */
+  #ok{
+    background-position: -202px 0px;
+  }
+  #ok:hover{
+    background-position: -202px -100px;
+  }
+  #ok:active{
+    background-position: -202px -200px;
+  }
+  /* good */
+  #good{
+    background-position: -303px 0px;
+  }
+  #good:hover{
+    background-position: -303px -100px;
+  }
+  #good:active{
+    background-position: -303px -200px;
+  }
+  /* excellent */
+  #excellent{
+    background-position: -404px 0px;
+  }
+  #excellent:hover{
+    background-position: -404px -100px;
+  }
+  #excellent:active{
+    background-position: -404px -200px;
   }
 </style>
